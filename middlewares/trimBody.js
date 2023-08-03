11842a51f72d6599e90cb5637eb1d650e16dc82b
unknown <sihristov@hotmail.com>
2023-08-03 20:09:51 +0300

    bugfix/fixing visual representatio when tirmming is involved

--- middlewares/trimBody.js
+++ middlewares/trimBody.js
@@ -1,11 +1,12 @@
-module.exports = (...excludedKeys) => (req, res, next) => {
-    if(req.body) {
-        for(let key in req.body) {
-            if(!excludedKeys.includes(key)) { //excludedKeys can be set in the expressController
-                req.body[key] = req.body[key].trim();
-            }
+module.exports =
+  (...excludedKeys) =>
+  (req, res, next) => {
+    if (req.body) {
+      for (let key in req.body) {
+        if (!excludedKeys.includes(key)) {
+          req.body[key] = req.body[key].trim();
         }
+      }
     }
-
     next();
-;}
+  };
--- static/htmlHelpers/textTrunc.js
+++ static/htmlHelpers/textTrunc.js
@@ -6,7 +6,7 @@ usernameElements.forEach((element) => {
   const originalText = element.textContent;
   const maxLength = 17; // Set the maximum length before truncation
 
-  if (originalText.length > maxLength) {
+  if (originalText.trim().length > maxLength) {
     const truncatedText = originalText.slice(0, maxLength) + '...';
     element.textContent = truncatedText;
     element.title = originalText; // Add the full text as a tooltip
