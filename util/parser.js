10545b6066d6c6a664d380a2b6e9ee955c62b0c6
unknown <sihristov@hotmail.com>
2023-10-19 23:01:55 +0300

    fixing backend bugs

--- util/parser.js
+++ util/parser.js
@@ -1,11 +1,23 @@
 function parseError(error) {
-    if(error.name == 'ValidationError') { //parsing errors from mongoose
-        return Object.values(error.errors).map(v => v.message);
+  const result = {
+    messages: [],
+    fields: {},
+  };
+
+  if (error.name == "ValidationError") {
+    for (let [field, e] of Object.entries(error.errors)) {
+      result.messages.push(e.message);
+      result.fields[field] = field;
     }
-    return error.message.split('\n');
+  } else if (Array.isArray(error)) {
+    result.messages = error.map((e) => e.msg);
+    result.fields = Object.fromEntries(error.map((e) => [e.param, e.param]));
+  } else {
+    result.messages = error.message.split("\n");
+  }
+  return result;
 }
 
 module.exports = {
-    parseError
+  parseError,
 };
-    
